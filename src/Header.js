9b4d8a8ec80ba0e7248dcf5b1ea0f6405273eeb9
Aaliyah <aaliyah.beckford97@gmail.com>
2021-03-11 02:05:55 -0500

    final commit

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <header>
             <div className="wrapper">
                 <h1> Anime Watch List</h1>
-                <p className="intro">Trying to get into anime and don't know where to start? Look up some anime below to get quick summaries and find your next bingeworthy anime. </p>
+                <p className="intro"> Look up some anime below to get quick summaries and find your next bingeworthy anime. </p>
 
             </div>
         </header>
